a2a30f57056e5eaca67008ac896885815771b8df
! j1nB0t.exe <166962541+jinbot073101@users.noreply.github.com>
2024-05-21 22:19:00 -0500

    Update MOTH.sh

--- MOTH.sh
+++ MOTH.sh
@@ -1,28 +1,91 @@
 #!/bin/bash
 
-read -p "Usuario: " hostName
-read -p "IP: " hostIP
-read -sp "Contraseña: " hostPass 
-echo
-read -p "Archivo a enviar: " filePath
+flag=$1
 
-remoteHost="$hostName@$hostIP"
+if [ -z "$flag" ]; then
 
-echo $remoteHost > keys.txt
+	read -p "Usuario: " hostName
+	read -p "IP: " hostIP
+	read -sp "Contraseña: " hostPass 
+	echo
+	read -p "Mensaje a enviar: " message
 
-remoteDesktopPath=$(sshpass -p "$hostPass" ssh "$remoteHost" 'echo $HOME/Desktop')
-remoteDesktopPath=$(echo "$remoteDesktopPath" | tr -d '[:space:]') 
+	remoteHost="$hostName@$hostIP"
 
-destinationPath="${remoteHost}:${remoteDesktopPath}"
+	filePath="$HOME/Desktop/message.txt"
+	echo $message > $filePath
 
-sshpass -p "$hostPass" scp "$filePath" "$destinationPath"
+	echo $remoteHost > keys.txt
 
-echo
-echo "Transferencia finalizada"
-echo
+	remoteDesktopPath=$(sshpass -p "$hostPass" ssh "$remoteHost" 'echo $HOME/Desktop')
+	remoteDesktopPath=$(echo "$remoteDesktopPath" | tr -d '[:space:]')
 
-fileName=$(basename "$filePath")
+	destinationPath="${remoteHost}:${remoteDesktopPath}"
 
-sshpass -p "$hostPass" ssh "$remoteHost" "${remoteDesktopPath}/notification.sh $fileName"
+	sshpass -p "$hostPass" scp "$filePath" "$destinationPath"
 
+	echo
+	echo "Transferencia finalizada"
+	echo
 
+	fileName=$(basename "$filePath")
+
+	sshpass -p "$hostPass" ssh "$remoteHost" "${remoteDesktopPath}/notification.sh $fileName"
+
+fi
+
+if [ $flag == '-s' ];then
+
+	echo "Ejemplo"
+
+        read -p "Usuario: " hostName
+        read -p "IP: " hostIP
+        read -sp "Contraseña: " hostPass
+        echo
+        read -p "Mensaje a enviar: " message
+
+        remoteHost="$hostName@$hostIP"
+	
+	filePath="$HOME/Desktop/message.txt"
+	echo $message > $filePath
+
+        echo $remoteHost > keys.txt
+
+        remoteDesktopPath=$(sshpass -p "$hostPass" ssh "$remoteHost" 'echo $HOME/Desktop')
+        remoteDesktopPath=$(echo "$remoteDesktopPath" | tr -d '[:space:]')
+
+        destinationPath="${remoteHost}:${remoteDesktopPath}"
+
+        sshpass -p "$hostPass" scp "$filePath" "$destinationPath"
+
+        echo
+        echo "Transferencia finalizada"
+        echo
+
+        fileName=$(basename "$filePath")
+
+        sshpass -p "$hostPass" ssh "$remoteHost" "${remoteDesktopPath}/notification.sh $fileName"
+	desktopEnv=$(sshpass -p "$hostPass" ssh "$remoteHost" 'echo $XDG_CURRENT_DESKTOP')
+	desktopEnv=$(echo "$desktopEnv" | tr -d '[:space:]')
+
+	case "$desktopEnv" in
+  	"GNOME")
+          editor="gedit"
+          ;;
+        "Cinnamon")
+          editor="xed"
+          ;;
+        "XFCE")
+          editor="mousepad"
+          ;;
+        "KDE")
+          editor="kate"
+          ;;
+        *)
+          editor="xed" 
+          ;;
+        esac
+
+	openMessage="$editor ${remoteDesktopPath}/message.txt"
+        sshpass -p "$hostPass" ssh "$remoteHost" "$openMessage"	
+fi
